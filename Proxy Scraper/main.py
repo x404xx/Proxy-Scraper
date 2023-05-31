f12aca08a4c4cd5b143c9c8a254faae09e0b3e30
x404xx <114883816+x404xx@users.noreply.github.com>
2023-06-01 06:37:43 +0800

    Update main.py

--- Proxy Scraper/main.py
+++ Proxy Scraper/main.py
@@ -1,54 +1,54 @@
-from banner import *
-from scraper import *
-from os import system
-system('cls')
+from os import name, system
+
+from api import SpysOne
+from banner import (
+    Colored,
+    ARROW,
+    DONE,
+    PROTOCOL_MENU,
+    QUANTITY_MENU,
+    SYMBOL,
+    TITLE,
+)
 
 
-def main():
-    if protocol == 1:
-        PROTO = 'http'
-        TYPE = '1'
-        print(f'\n{symb} {green}Scraping HTTP now..{reset}\n')
-        if (proxy := Scraper(PROTO, TYPE, QUANTITY)):
-            with open('http.txt', 'w', encoding='utf-8') as result:
-                for prox in proxy:
-                    print(prox)
-                    result.write(f'http://{prox}\n')
-                print(f'''
-{strg}
-        {done}''')
+def scraper(info=False):
+    protocol_mapping = {
+        1: ('1', 'http', 'http.txt', 'http://'),
+        2: ('2', 'socks', 'socks5.txt', 'socks5://')
+    }
+    quantity_mapping = {
+        1: '0',
+        2: '1',
+        3: '2',
+        4: '3',
+        5: '4',
+        6: '5'
+    }
 
-    elif protocol == 2:
-        PROTO = 'socks'
-        TYPE = '2'
-        print(f'\n{symb} {green}Scraping SOCKS now..{reset}\n')
-        if (proxy := Scraper(PROTO, TYPE, QUANTITY)):
-            with open('socks5.txt', 'w', encoding='utf-8') as result:
-                for prox in proxy:
-                    print(prox)
-                    result.write(f'socks5://{prox}\n')
-                print(f'''
-{strg}
-        {done}''')
+    system('cls' if name == 'nt' else 'clear')
+    print(f'{TITLE}\n{ARROW}')
 
-    else: print(f'\n{red}Not Exist!{reset}\n')
+    protocol_option = int(input(PROTOCOL_MENU))
+    if protocol_option in protocol_mapping:
+        proxy_number, proxy_type, file_name, prefix = protocol_mapping[protocol_option]       
+        quantity_option = int(input(QUANTITY_MENU))
+        quantity = quantity_mapping.get(quantity_option)
+        
+        print(f'\n{SYMBOL} {Colored.GREEN}Scraping {proxy_type.upper()} now..{Colored.RESET}\n')
+        
+        with open(file_name, 'w', encoding='utf-8') as file:
+            proxies = SpysOne(proxy_type, proxy_number, quantity, info)
+            if (result := proxies.scrape()):
+                for proxy in result:
+                    print(proxy)
+                    file.write(f'{prefix}{proxy}\n')
+                print(f'\n{ARROW}\n{DONE}')
+    else:
+        print(f'\n{Colored.RED}Please check your input!{Colored.RESET}\n')
 
 
 if __name__ == '__main__':
-
-    print(f'''{title}
-{strg}''')
-
-    protocol = int(input(proc))
-    quantity = int(input(qtt))
-    if quantity == 1: QUANTITY = '0'
-    elif quantity == 2: QUANTITY = '1'
-    elif quantity == 3: QUANTITY = '2'
-    elif quantity == 4: QUANTITY = '3'
-    elif quantity == 5: QUANTITY = '4'
-    elif quantity == 6: QUANTITY = '5'
-    else: print(f'\n{red}Not Exist!{reset}\n')
-    main()
-
-
-
+    #! If FALSE (194.233.78.142:49653)
+    #! If TRUE (194.233.78.142:49653 - 31-may-2023 - 23:15 (2 hours ago))
+    scraper(info=False)
